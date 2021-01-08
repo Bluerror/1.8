df208f449859e1da7d33110cc27033bcedaa8f73
Lenovo <Lenovo@LAPTOP-8E12VNVR>
2021-01-08 22:13:07 +0800

    添加项目文件。

---
+++ 1.8/1.cpp
@@ -0,0 +1,63 @@
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//using namespace cv;
+//using namespace std;
+//
+//const int MAX_CLUSTERS = 5;
+//Scalar colorTab[] = {
+//	Scalar(0,0,2555),
+//	Scalar(0,255,0),
+//	Scalar(255,100,100),
+//	Scalar(255,0,255),
+//	Scalar(0,255,255)
+//};
+//
+//Mat img(500, 500, CV_8UC3);
+//RNG rng(12345);
+//
+//int main()
+//{
+//	int k, clusterCount = rng.uniform(2, MAX_CLUSTERS + 1);
+//	int i, sampleCount = rng.uniform(1, 1001);
+//	Mat points(sampleCount, 1, CV_32FC2), labels;
+//
+//	clusterCount = MIN(clusterCount, sampleCount);
+//	std::vector<Point2f>centers;
+//
+//	for (k = 0; k < clusterCount; k++)
+//	{
+//		Point center;
+//		center.x = rng.uniform(0, img.cols);
+//		center.y = rng.uniform(0, img.rows);
+//		Mat pointChunk = points.rowRange(k * sampleCount / clusterCount,
+//			k == clusterCount - 1 ? sampleCount:
+//		(k+1)*sampleCount/clusterCount);
+//		rng.fill(pointChunk,
+//			RNG::NORMAL,
+//			Scalar(center.x, center.y),
+//			Scalar(img.cols * 0.05, img.rows * 0.05));
+//	}
+//	randShuffle(points, 1, &rng);
+//	double compactness = kmeans(points ,clusterCount,labels,
+//		TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 1.0),
+//		3, KMEANS_PP_CENTERS, centers);
+//
+//	img = Scalar::all(0);
+//
+//	for (i = 0; i <sampleCount; i++)
+//	{
+//		int clusterIdx = labels.at<int>(i);
+//		Point ipt = points.at<Point2f>(i);
+//		circle(img, ipt, 2, colorTab[clusterIdx], FILLED, LINE_AA);
+//	}
+//	for ( i = 0; i < (int)centers.size(); ++i)
+//	{
+//		Point2f s = centers[i];
+//		circle(img, s, 40,colorTab[i],1, LINE_AA);
+//	}
+//   
+//	imshow("original", img);
+//	
+//	waitKey(0);
+//	return 0;
+//}
